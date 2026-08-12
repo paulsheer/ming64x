baa71da249eaa1f881a56948a155e8f43b02bb56
Paul Sheer <paulsheer@gmail.com>
2026-08-12 23:09:46 +0000

    Remove unnecessary 10ms timeout

--- xorg-server/os/WaitFor.c
+++ xorg-server/os/WaitFor.c
@@ -201,6 +201,7 @@ WaitForSomething(Bool are_ready)
             i = -1;
         else
         {
+//             if (!timeout) timeout=10; /* Avoid a 100 % usage loop, timeout is in milliseconds */
             i = ospoll_wait(server_poll, timeout);
         }
         pollerr = GetErrno();
