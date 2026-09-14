264bfc4db5e5a39c77cde486f33326403b8e7b5b
Paul Sheer <paulsheer@gmail.com>
2026-09-14 00:43:57 +0000

    dix: assert that size of buffers to swap is a multiple of the swap size
    If we're swapping 4-byte integers or 2-byte integers, make sure the size
    of the buffer doesn't have any bytes left over, since we won't correctly
    handle those bytes.
    
    Reported in #1817:
    
    xwayland-24.1.6/redhat-linux-build/../dix/swaprep.c:99:22:
     warning[-Wanalyzer-allocation-size]:
     allocated buffer size is not a multiple of the pointee's size
    
    xwayland-24.1.6/redhat-linux-build/../dix/swaprep.c:146:22:
     warning[-Wanalyzer-allocation-size]:
     allocated buffer size is not a multiple of the pointee's size
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    (cherry picked from commit cf49354b6060b71ae41febe67327278fbcb7c74a)
    
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2077>
    
    Was 236e712ffbfd from gitlab.freedesktop.org/xorg/xserver.git

--- xorg-server/dix/swaprep.c
+++ xorg-server/dix/swaprep.c
@@ -46,6 +46,8 @@ SOFTWARE.
 
 #include <dix-config.h>
 
+#include <assert.h>
+
 #include <X11/X.h>
 #include <X11/Xproto.h>
 #include "misc.h"
@@ -93,6 +95,8 @@ CopySwap32Write(ClientPtr pClient, int size, CARD32 *pbuf)
     CARD32 *from, *to, *fromLast, *toLast;
     CARD32 tmpbuf[1];
 
+    assert((bufsize % sizeof(CARD32)) == 0);
+
     /* Allocate as big a buffer as we can... */
     while (!(pbufT = malloc(bufsize))) {
         bufsize >>= 1;
@@ -139,6 +143,8 @@ CopySwap16Write(ClientPtr pClient, int size, short *pbuf)
     short *pbufT;
     short *from, *to, *fromLast, *toLast;
     short tmpbuf[2];
+
+    assert((bufsize % sizeof(short)) == 0);
 
     /* Allocate as big a buffer as we can... */
     while (!(pbufT = malloc(bufsize))) {
