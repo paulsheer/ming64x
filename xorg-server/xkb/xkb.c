20949925937a5aa98316deac40a33bb47a488a84
Paul Sheer <paulsheer@gmail.com>
2026-09-14 00:44:00 +0000

    xkb: Make the RT_XKBCLIENT resource private
    Currently, the resource in only available to the xkb.c source file.
    
    In preparation for the next commit, to be able to free the resources
    from XkbRemoveResourceClient(), make that variable private instead.
    
    This is related to:
    
    CVE-2025-62230, ZDI-CAN-27545
    
    This vulnerability was discovered by:
    Jan-Niklas Sohn working with Trend Micro Zero Day Initiative
    
    Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>
    Reviewed-by: Michel Dänzer <mdaenzer@redhat.com>
    (cherry picked from commit 99790a2c9205a52fbbec01f21a92c9b7f4ed1d8f)
    
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2087>
    Was 865089ca7084 from gitlab.freedesktop.org/xorg/xserver.git

--- xorg-server/xkb/xkb.c
+++ xorg-server/xkb/xkb.c
@@ -53,7 +53,7 @@ int XkbKeyboardErrorCode;
 CARD32 xkbDebugFlags = 0;
 static CARD32 xkbDebugCtrls = 0;
 
-static RESTYPE RT_XKBCLIENT;
+RESTYPE RT_XKBCLIENT = 0;
 
 /***====================================================================***/
 
